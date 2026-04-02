7fc57e993cf69fc75ecb84ffe30a28d14b41bba5
Ibragimov Jasurbek <morgan7122010@gmail.com>
2026-04-02 12:44:55 +0300

    Update UYGA VAZIFA 11.py

--- UYGA VAZIFA 11.py
+++ UYGA VAZIFA 11.py
@@ -1,3 +1,58 @@
+def mijoz_malumotlari(ism, familiya, tugilgan_yil, tugilgan_joy, email=None, tel=None):
+    """Mijoz haqidagi ma'lumotlarni lug'at ko'rinishida qaytaruvchi funksiya"""
+    mijoz = {
+        'ism': ism,
+        'familiya': familiya,
+        'yosh': 2024 - tugilgan_yil,
+        'tugilgan_joy': tugilgan_joy,
+        'email': email,
+        'telefon': tel
+    }
+    return mijoz
+
+print(mijoz_malumotlari("Ali", "Valiyev", 1995, "Toshkent", email="ali@mail.com"))
+
+mijozlar = []
+while True:
+    print("\nMijoz ma'lumotlarini kiriting:")
+    ism = input("Ismi: ")
+    familiya = input("Familiyasi: ")
+    t_yil = int(input("Tug'ilgan yili: "))
+    t_joy = input("Tug'ilgan joyi: ")
+    email = input("Email (ixtiyoriy): ")
+    tel = input("Telefon (ixtiyoriy): ")
+    
+    mijozlar.append(mijoz_malumotlari(ism, familiya, t_yil, t_joy, email, tel))
+       
+    javob = input("Yana mijoz qo'shasizmi? (ha/yo'q): ")
+    if javob.lower() != 'ha':
+        break
+
+print("\nRo'yxatdagi mijozlar:")
+for mijoz in mijozlar:
+    print(f"{mijoz['ism'].title()} {mijoz['familiya'].title()}, "
+          f"{mijoz['yosh']} yoshda, tel: {mijoz['telefon']}")
+
+def kattasini_top(x, y, z):
+    """Uchta sondan eng kattasini qaytaruvchi funksiya"""
+    if x >= y and x >= z:
+        return x
+    elif y >= x and y >= z:
+        return y
+    else:
+        return z
+
+print(f"Eng katta son: {kattasini_top(15, 28, 10)}")
+
+def aylana_malumotlari(radius, pi=3.14159):
+    """Aylana haqidagi ma'lumotlarni hisoblovchi funksiya"""
+    aylana = {
+        'radius': radius,
+        'diametr': 2 * radius,
+        'perimetr': 2 * pi * radius,
+        'yuzi': pi * (radius ** 2)
+    }
+    return aylana
 
 print(aylana_malumotlari(5))
 
