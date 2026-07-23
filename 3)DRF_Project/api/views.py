655e7622f48c6f2fba69602282dc6c21dfeb5bca
Ibragimov Jasurbek <morgan7122010@gmail.com>
2026-07-23 14:00:18 +0500

    Delete 3)DRF_Project directory

--- 3)DRF_Project/api/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from rest_framework.viewsets import ModelViewSet
-from .models import Author
-from .serializera import AuthorSerializer
-# Create your views here.
-
-class AuthorViewSet(ModelViewSet):
-    queryset = Author.objects.all()
-    serializer_class = AuthorSerializer
